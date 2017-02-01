171c95b07349bfe9f1e1994ed3e8ce8c16654e24
kiranhsgithub <hskiran.bms@gmail.com>
2017-02-01 16:05:04 -0500

    functions

--- Ex_Files_UaR_Python/Ch2/functions_start.py
+++ Ex_Files_UaR_Python/Ch2/functions_start.py
@@ -28,3 +28,21 @@ def cube(x):
 
 print(cube(3))
     
+def power(num, x=1):
+    result = 1;
+    for i in range(x):
+        result = result * num
+    return result
+
+print(power(2))
+print(power(2,3))
+print(power(x=2, num=5))
+
+def multi_add(*args):
+    result=0;
+    for x in args:
+        result = result + x
+    return result
+
+print(multi_add(4,5,10,4))
+print(multi_add(4,5,10,4,10))
