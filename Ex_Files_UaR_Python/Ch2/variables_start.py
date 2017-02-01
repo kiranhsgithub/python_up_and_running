bad110d625ebb1bf68f9206afd2020bacecccb4a
kiranhsgithub <hskiran.bms@gmail.com>
2017-02-01 14:17:49 -0500

    strin concatenation

--- Ex_Files_UaR_Python/Ch2/variables_start.py
+++ Ex_Files_UaR_Python/Ch2/variables_start.py
@@ -2,3 +2,18 @@
 # Example file for variables
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
+f = 0;
+print(f)
+
+f = "abc"
+print(f)
+
+#print("String type " + 123)
+
+print("String type " + str(123))
+
+def someFunction():
+    f = "def"
+    print f
+    
+    
