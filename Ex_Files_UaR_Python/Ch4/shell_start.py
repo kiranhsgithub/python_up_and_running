4aa97ff294bae6370b1603c626301c55372884fa
kiranhsgithub <hskiran.bms@gmail.com>
2017-02-02 03:18:54 -0500

    shell utils

--- Ex_Files_UaR_Python/Ch4/shell_start.py
+++ Ex_Files_UaR_Python/Ch4/shell_start.py
@@ -2,7 +2,18 @@
 # Example file for working with filesystem shell methods
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
+
+import os
+import shutil
+from os import path
+
 def main():
+    if path.exists("textfile.txt"):
+        src = path.realpath("textfile.txt");
+        
+    head ,tail = path.split(src)
+    print("path: ", head)
+    print("file: ", tail)
       
 if __name__ == "__main__":
   main()
