221c3a225469756787942de48af1751f076126fe
kiranhsgithub <hskiran.bms@gmail.com>
2017-02-02 03:41:12 -0500

    python shell utils

--- Ex_Files_UaR_Python/Ch4/shell_start.py
+++ Ex_Files_UaR_Python/Ch4/shell_start.py
@@ -6,6 +6,8 @@
 import os
 import shutil
 from os import path
+from shutil import make_archive
+from zipfile import ZipFile
 
 def main():
     if path.exists("textfile.txt"):
@@ -14,6 +16,22 @@ def main():
     head ,tail = path.split(src)
     print("path: ", head)
     print("file: ", tail)
+    
+    dst = src + ".bak"
+    shutil.copy(src,dst)
+    
+    dst2 = src + ".bak2"
+    shutil.copy(src,dst2)
+    shutil.copystat(src,dst2)
+    
+    #os.rename(src + ".bak2","newfile.txt")
+    
+    root_dir, tail = path.split(src)
+    shutil.make_archive("archive", "zip", root_dir)
+    
+    with ZipFile("testing.zip", "w") as newzip:
+        newzip.write("textfile.txt")
+        newzip.write("textfile.txt.bak")
       
 if __name__ == "__main__":
   main()
