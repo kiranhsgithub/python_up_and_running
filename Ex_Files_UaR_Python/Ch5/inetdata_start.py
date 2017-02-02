5c7e1f7ccf55bf204a953a52c31b4072b8ff448b
kiranhsgithub <hskiran.bms@gmail.com>
2017-02-02 04:23:29 -0500

    json parsing

--- Ex_Files_UaR_Python/Ch5/inetdata_start.py
+++ Ex_Files_UaR_Python/Ch5/inetdata_start.py
@@ -2,5 +2,14 @@
 # Example file for retrieving data from the internet
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
+import urllib.request
+
+def main():
+    webUrl = urllib.request.urlopen("http://joemarini.com")
+    
+    print("result code: " + str(webUrl.getcode()))
+    
+    data = webUrl.read()
+    print(data)
 if __name__ == "__main__":
   main()
--- Ex_Files_UaR_Python/Ch5/jsondata_start.py
+++ Ex_Files_UaR_Python/Ch5/jsondata_start.py
@@ -2,13 +2,45 @@
 # Example file for parsing and processing JSON
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
-  
-  
+import urllib.request
+import json
+
+def printResults(data):
+    theJSON = json.loads(data)
+    
+    if "title" in theJSON["metadata"]:
+        print(theJSON["metadata"]["title"])
+    
+    count = theJSON["metadata"]["count"];
+    print(str(count) + "events recorded")
+    
+    for i in theJSON["features"]:
+        print(i["properties"]["place"])
+        
+    for i in theJSON["features"]:
+        if i["properties"]["mag"] >= 4.0:
+            print("%2.1f" % i["properties"]["mag"], i["properties"]["place"])
+            
+    print("Events that were felt")
+    for i in theJSON["features"]:
+        feltReports = i["properties"]["felt"]
+        if feltReports != None:
+            if feltReports > 0:
+                print("%2.1f" % i["properties"]["mag"], i["properties"]["place"], " reported " + str(feltReports) + " times")
+    
 def main():
-  # define a variable to hold the source URL
-  # In this case we'll use the free data feed from the USGS
-  # This feed lists all earthquakes for the last day larger than Mag 2.5
-  urlData = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
+    # define a variable to hold the source URL
+    # In this case we'll use the free data feed from the USGS
+    # This feed lists all earthquakes for the last day larger than Mag 2.5
+    urlData = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
+    
+    webUrl = urllib.request.urlopen(urlData)
+    print(webUrl.getcode())
+    if(webUrl.getcode() == 200):
+        data = webUrl.read()
+        printResults(data)
+    else:
+        print("Received an error from server, cannot retrieve results " + str(webUrl.getcode()))
   
 
 if __name__ == "__main__":
